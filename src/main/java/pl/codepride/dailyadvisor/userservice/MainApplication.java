24fbd4010f3c8f4e0cfbd9079de8922868718d40
makowskimarek <makow.mark@gmail.com>
2018-11-06 19:55:54 +0100

    test registration

--- src/main/java/pl/codepride/dailyadvisor/userservice/MainApplication.java
+++ src/main/java/pl/codepride/dailyadvisor/userservice/MainApplication.java
@@ -2,10 +2,10 @@ package pl.codepride.dailyadvisor.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableAsync
 public class MainApplication {
 
 	public static void main(String[] args) {
--- src/main/java/pl/codepride/dailyadvisor/userservice/configuration/SecurityConfiguration.java
+++ src/main/java/pl/codepride/dailyadvisor/userservice/configuration/SecurityConfiguration.java
@@ -94,15 +94,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
                 .addFilterBefore(filterChain(), UsernamePasswordAuthenticationFilter.class)
                 .logout().disable()
-                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).requireCsrfProtectionMatcher(
-                new NegatedRequestMatcher(
-                        new OrRequestMatcher(
-                                new AntPathRequestMatcher("/login/facebook"),
-                                new AntPathRequestMatcher("/login/google"),
-                                new AntPathRequestMatcher("/login/oauth2")
-                        )
-                ))
-                .and()
+                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/").permitAll()
                 .antMatchers("/afterLogin").permitAll()
@@ -134,7 +126,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     public void configure(WebSecurity web) {
         web
                 .ignoring()
-                .antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**", "/csrf**", "/populate");
+                .antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**", "/csrf", "/populate");
     }
 
     private Filter filterChain() throws Exception {
